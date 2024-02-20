4dfd397d1e1a2cab47c6ad9d4ca87f74e4079eef
rijuly <136062565+rijuly@users.noreply.github.com>
2024-02-20 18:09:44 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ var names_of_people = [];
 function submit()
 {
     var GuestName = document.getElementById("name1").value;
-	// use the push function as - names_of_people.push(GuestName);
+	 names_of_people.push(GuestName);
 	console.log(GuestName);    
     console.log(names_of_people);
-    var lenght_of_name = names_of_people.length;
-    console.log(lenght_of_name);
+    var length_of_name = names_of_people.length;
+    console.log(length_of_name);
 	document.getElementById("display_name").innerHTML=names_of_people.toString();
    }
 
@@ -25,8 +25,7 @@ function show()
 
 function sorting()
 	{
-		names_of_people           // add the sort function here
-		// .sort();
+		names_of_people.sort();          
 		var i= names_of_people.join("<br>");
 		console.log(names_of_people);		
 		document.getElementById("sorted").innerHTML=i.toString();
